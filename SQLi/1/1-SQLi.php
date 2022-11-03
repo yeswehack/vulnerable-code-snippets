baf21c6f339fa296ad0bd3e1a66c2d39e845c69d
brumensywh <113608243+brumensywh@users.noreply.github.com>
2022-11-03 11:18:37 +0100

    Create 1-SQLi.php

---
+++ SQLi/1/1-SQLi.php
@@ -0,0 +1,30 @@
+<?php
+ //Note, this should be the path to the "db.php" file that is in the root of the repo + credentials added:
+include("../../db.php");
+
+$query = $_GET['q'];
+$lst_blacklist = array("\"", "`", "'", "\\" );
+
+foreach ( $lst_blacklist as $char ) {
+  if ( in_array($char, $lst_blacklist) ) {
+    $query = str_replace($char, ("\\".$char), $query);
+  }
+}
+
+$sql = "SELECT * FROM `products` WHERE category = '$query'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "Id:" . $row["id"] . "\n",
+      "Stock: " . $row["stock"] . "\n",
+      "Category: " .$row["category"] . "\n",
+      "Color: " .$row["color"] . "\n----\n";
+    }
+} else {
+        echo "0 results for $query";
+}
+
+$conn->close();
+?> 
