2a54eed150a3c20190eb74cf4c0ceffca47b91e7
brumensywh <113608243+brumensywh@users.noreply.github.com>
2022-11-03 11:26:08 +0100

    Create 3-RCE.php

---
+++ RCE/1/3-RCE.php
@@ -0,0 +1,40 @@
+<?php
+
+function IncludeFilter($str) {
+    /**Secure the input from path traversal:
+    */
+    while (True) {
+        if ( !str_contains($str, "../") ) {
+            break;
+        }
+        $str = str_replace("../", "", $str);
+    }
+
+    return $str;
+}
+
+function OSPath($str){
+    /**Normalize slash related to OS:
+    */
+    if ( strtolower(PHP_OS) == "linux" ) {
+        $str = str_replace("\\", "/", $str);
+        
+    } else {
+        $str = str_replace("/", "\\", $str);
+    }
+
+    return $str;
+}
+
+$lang = $_GET['lang'];
+
+include(OSPath("home/" . IncludeFilter($lang)));
+
+if ( $lang != "en" && $lang != "fr" ) {
+    file_put_contents("logs/log.txt", $lang, FILE_APPEND);
+    echo "<b>Invalid language</b>";
+
+    include "home/en";
+}
+
+?>
