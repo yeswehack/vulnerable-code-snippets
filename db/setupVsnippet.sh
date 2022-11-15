06a840a81c65ff209cd01232aa4e616406bf910e
brumensywh <113608243+brumensywh@users.noreply.github.com>
2022-11-15 17:55:05 +0100

    Update setupVsnippet.sh

--- db/setupVsnippet.sh
+++ db/setupVsnippet.sh
@@ -15,7 +15,7 @@ if [[ $1 == "" || $2  == "" || $3  == "" ]];then
     exit;
 fi;
 
-echo -e "[\033[36mINF\033[0m] Is this correct? ( [CTRL+C] to cancel) Waiting 3 secounds... before continue";
+echo -e "[\033[36mINF\033[0m] Is this correct? ( [CTRL+C] to cancel) Waiting 3 secounds...";
 sleep 3;
 
 lst=('db.php' 'db.py' 'db.js');
@@ -24,7 +24,7 @@ for i in "${lst[@]}";do
 done;
 
 echo -e "[\033[1;32mOK\033[0m] Insert credentials to 'setup.sql';
-[\033[36mINF\033[0m] To make MySQL changes you need to enter the sudo password (\033[3;33mnot the user your about to create\033[0m)";
+[\033[36mINF\033[0m] To make the MySQL changes, you need to enter the sudo password (\033[3;33mnot the user your about to create\033[0m)";
 
 #Add credentials and run SQL setup: 
 sed -i "s/__USER__/$1/;s/__PASS__/$2/;s/__DB__/$3/" setup.sql;
