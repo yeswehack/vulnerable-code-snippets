1e8755811dab176499cf8984c8191730d407310f
brumensywh <113608243+brumensywh@users.noreply.github.com>
2022-11-15 14:25:43 +0100

    Create db.js

---
+++ db/db.js
@@ -0,0 +1,14 @@
+var mysql = require('mysql');
+
+/*
+* YesWeHack - Vulnerable code snippets
+*/
+
+con = mysql.createConnection({
+  database: "<DATABASE>",
+  host: "<HOST>",
+  user: "<USERNAME>",
+  password: "<PASSWORD>",
+}); */
+
+//con.connect();
