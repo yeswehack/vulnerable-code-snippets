ae9991a46d8467b63f48ddf3e5428257c4e477b6
brumensywh <113608243+brumensywh@users.noreply.github.com>
2022-11-16 15:58:22 +0100

    Update db.js

--- db/db.js
+++ db/db.js
@@ -5,10 +5,10 @@
 var mysql = require('mysql');
 
 con = mysql.createConnection({
-  database: "__DB__",
-  host: "localhost",
-  user: "__USER__",
-  password: "__PASS__",
+  database: '__DB__',
+  host: 'localhost',
+  user: '__USER__',
+  password: '__PASS__',
 }); */
 
 //con.connect();
