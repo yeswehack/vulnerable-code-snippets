97eb985a9981759186ae61ff7722ba9a99d1e714
Brumens <113608243+brumensywh@users.noreply.github.com>
2023-05-08 10:05:20 +0200

    Create 30-deserialization.php

---
+++ Deserialization/2/30-deserialization.php
@@ -0,0 +1,37 @@
+<?php
+//Only design (ignore)
+include_once('../../design/design.php');
+Design(__FILE__, "30 - Vsnippet");
+
+//
+// YesWeHack - Vulnerable Code Snippet
+//
+//Run: php -S 127.1:5000 30-new.php
+
+class Client {
+    private $ip;
+    private $created;
+
+    function __construct() {
+        $this->ip = $_SERVER['HTTP_X_CLIENT_IP'];
+        $this->created = date('m/d/Y h:i:s a', time());
+        //Code...
+    }
+    //Measure latency:
+    function __wakeup() {
+        $data = exec("ping -c 4 -W 5 -n " . $this->ip);
+        echo "Result: $data \n";
+        //Code...
+    }
+}
+
+if ( !isset($_COOKIE['client']) || strlen($_COOKIE['client']) == 0 ) {
+    $client = new Client();
+    setcookie("client", base64_encode(serialize($client)), ['httponly' => true]);
+} else {
+    $client_data = base64_decode(urldecode($_COOKIE['client']));
+    unserialize($client_data);
+    //Code...
+}
+
+?>
