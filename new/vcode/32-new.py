4b086895d048ac3a72c75ed0dde707dca6b00ac3
Brumens <113608243+brumensywh@users.noreply.github.com>
2023-06-16 15:07:14 +0200

    Update and rename 31-new.php to 32-new.py

---
+++ new/vcode/32-new.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+from flask import Flask, json, request
+import datetime
+app = Flask(__name__)
+
+##
+# YesWeHack - Vulnerable code snippets
+##
+
+@app.route('/items')
+def index():
+
+    id = request.args.get('id')
+    msg = f"Could not find item for {id}"
+
+    #Extract all items from our JSON file:
+    with open('items.json', 'r') as items:
+        data = json.load(items)
+       
+        if id is None:
+            return data
+
+        elif (id is not None) and (id in data.keys()):
+            return data[id]
+
+        else:
+            return json.loads( ('{{ "{0}":"'+msg+'" }}').format(datetime.datetime.now()) )
+
+
+if __name__=='__main__':
+    app.run(debug=True)
