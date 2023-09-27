cd79c0fa8ba4c920349a569456b0ad83709e6605
brumensywh <113608243+brumensywh@users.noreply.github.com>
2023-09-27 14:17:53 +0100

    Vsnippet 32 - Format injection got dockerized

---
+++ FormatInjection/1/vsnippet/32-formatinjection.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+from flask import Flask, json, render_template, request
+import datetime
+from ignore.design import design
+app = design.Design(Flask(__name__), __file__, 'Vsnippet 32 - Format injection')
+
+
+
+##
+# YesWeHack - Vulnerable code snippets
+##
+
+@app.route('/')
+def index():
+
+    id = request.args.get('id')
+    msg = f"Could not find item for {id}"
+
+    #Extract all items from our JSON file:
+    with open('items.json', 'r') as items:
+        data = json.load(items)
+
+        if id is None:
+            return render_template('index.html', result="Error")
+
+        elif (id is not None) and (id in data.keys()):
+            return render_template('index.html', result=str(data[id]))
+
+        else:
+            return render_template('index.html', result=json.loads( ('{{ "{0}":"'+msg+'" }}').format(datetime.datetime.now()) ))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=1337, debug=True)
